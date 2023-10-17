36f0ffb9aa88ab066626b67d7cad09f9b0ff1889
Z6O9D <95990845+Z6O9D@users.noreply.github.com>
2023-10-17 13:12:09 -0300

    Add files via upload

--- test 1.py
+++ test 1.py
@@ -1,18 +1,9 @@
-def buscar(letra):
-    fila=0
-    for i in range(len(matriz)):
-        if letra in matriz[i]:
-            fila=i
-
-    columna=matriz[fila].index(letra)
-    return (fila,columna)
-
-
 
 
 class laberinto:
     class Nodo:
-        def __init__(self,x,y):
+        def __init__(self,x,y,info):
+            self.info = info
             self.x = x
             self.y = y
             self.arriba = None
@@ -20,20 +11,37 @@ class laberinto:
             self.derecha = None
             self.izquierda = None
         def conectar(self,nodo,i,j):
-            if self.y > i: 
+            if self.y == i + 1: 
                 self.arriba = nodo
-            if self.y < i:
+            if self.y + 1 == i:
                 self.abajo = nodo
-            if self.x < j:
+            if self.x + 1 == j:
                 self.derecha = nodo
-            if self.x > j:
+            if self.x == j+1:
                 self.izquierda = nodo
-    def __init__(self,inicio):
-        start = buscar(inicio)
-        self.visitados = []
-        self.inicio = self.Nodo(start[0],start[1])
-        self.addvisitado(start)
+            else:
+                print("El nodo no es adyacente")
+    def __init__(self,matriz):
+        self.matriz=matriz
         self.caminos=[]
+        self.info = {}
+        self.inicio = None
+        self.fin = None
+    def buscar(self):◘
+        for i in range(len(self.matriz)):
+            for j in range(len(self.matriz[i])):
+                casilla = self.matriz[i][j]
+                if casilla == ".":
+                    self.info[str(i)+","+str(j)] = self.Nodo(i,j,casilla)
+                elif casilla == "#":
+                    pass
+                elif casilla == ".":
+                    self.info[casilla] = self.Nodo(i,j,casilla)
+                else:
+                    if casilla in self.info["portal"]:
+                        self.info["portal"][casilla] = self.Nodo(i,j,casilla)
+                    else:
+                        self.info["portal"][casilla] = [self.Nodo(i,j,casilla)]
 
     def addvisitado(self,tuple):
         self.visitados.append(tuple)
@@ -49,8 +57,28 @@ class laberinto:
         return nodo.izquierda
     def posibles(self):
         return self.caminos
-    def test(self,objetivo,matriz):
+    def test_sinportal(self,matriz):
         pass
+    def test_gota(self,matriz):
+        evaluar=[self.inicio]
+        evaluados=[]
+        for i in range(len(tablero)):
+            for j in range(len(tablero)):
+                if tablero[i][j] == 0 and (i>0 or i<len(tablero)) and (j>0 or j<len(tablero)):
+                    for k in range(-1,2):
+                        for l in range(-1,2):
+                            try:
+                                if tablero[i+k][j+l] == "." and (i+k > -1 and j+l > -1):
+                                    tablero[i+k][j+l] = minefield[i+k][j+l]
+                                    band=True
+                            except IndexError:
+                                pass
+            if band:
+                cero(tablero,minefield)
+
+
+
+
 
 
 
