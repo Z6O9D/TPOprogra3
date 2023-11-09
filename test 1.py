2ead4fde73d7fe92e237fd3cc94ef843e7705592
Z6O9D <95990845+Z6O9D@users.noreply.github.com>
2023-11-09 02:47:56 -0300

    Add files via upload

--- test 1.py
+++ test 1.py
@@ -1,107 +1,134 @@
+def search(pos_inicial,find):
+    direccion=[]
+    if pos_inicial[0] - find[0]<0:
+        direccion.append(1)
+    elif pos_inicial[0] - find[0]>0:
+        direccion.append(-1)
+    else:
+        direccion.append(0)
+    if pos_inicial[1] - find[1]<0:
+        direccion.append(1)
+    elif pos_inicial[1] - find[1]>0:
+        direccion.append(-1)
+    else:
+        direccion.append(0)
+    return direccion
+
+def mostrar_conexiones(objeto):
+    for key in objeto.portales.values():
+        print(key[0].sig)
 
 
+class Nodo:
+    def __init__(self, letra,pos, cambio):
+        self.letra = letra
+        self.pos = pos
+        self.sig = []
+        self.sig_peso = {}
+        self.cambio = cambio
+    def add_next(self, nodo_siguiente, peso_arista):
+        self.sig.append(nodo_siguiente)
+        self.sig_peso[nodo_siguiente.letra] = peso_arista
 class laberinto:
-    class Nodo:
-        def __init__(self,x,y,info):
-            self.info = info
-            self.x = x
-            self.y = y
-            self.arriba = None
-            self.abajo = None
-            self.derecha = None
-            self.izquierda = None
-        def conectar(self,nodo,i,j):
-            if self.y == i + 1: 
-                self.arriba = nodo
-            if self.y + 1 == i:
-                self.abajo = nodo
-            if self.x + 1 == j:
-                self.derecha = nodo
-            if self.x == j+1:
-                self.izquierda = nodo
-            else:
-                print("El nodo no es adyacente")
     def __init__(self,matriz):
-        self.matriz=matriz
-        self.caminos=[]
-        self.info = {}
+        self.portal_list = []
+        self.matriz = matriz
+        self.matriz_peso = []
+        self.caminos_posibles = {}
+        self.portales = {}
         self.inicio = None
-        self.fin = None
-    def buscar(self):◘
-        for i in range(len(self.matriz)):
-            for j in range(len(self.matriz[i])):
-                casilla = self.matriz[i][j]
-                if casilla == ".":
-                    self.info[str(i)+","+str(j)] = self.Nodo(i,j,casilla)
-                elif casilla == "#":
-                    pass
-                elif casilla == ".":
-                    self.info[casilla] = self.Nodo(i,j,casilla)
-                else:
-                    if casilla in self.info["portal"]:
-                        self.info["portal"][casilla] = self.Nodo(i,j,casilla)
+        self.salida = None
+        for i in range(len(matriz)):
+            for j in range(len(matriz[i])):
+                casilla = matriz[i][j]
+                if casilla not in [".", "#"]:
+                    if casilla == "E":
+                        self.inicio = Nodo(casilla,[i,j],None)
+                    elif casilla == "S":
+                        self.salida = Nodo(casilla,[i,j],None)
+                    elif casilla not in self.portal_list:
+                        self.portal_list.append(casilla)
+                        pos = [i,j]
+                        self.portales[casilla] = []
+                        cambiar=self.buscar_cambio(casilla, pos)
+                        self.portales[casilla].append(Nodo(casilla,[i,j],cambiar))
                     else:
-                        self.info["portal"][casilla] = [self.Nodo(i,j,casilla)]
+                        cambiar=self.buscar_cambio(casilla, pos)
+                        self.portales[casilla].append(Nodo(casilla,[i,j],cambiar))
 
-    def addvisitado(self,tuple):
-        self.visitados.append(tuple)
-    def addcamino(self,tuple):
-        self.visitados.append(tuple)
-    def arriba(nodo):
-        return nodo.arriba
-    def abajo(nodo):
-        return nodo.abajo
-    def derecha(nodo):
-        return nodo.derecha
-    def izquierda(nodo):
-        return nodo.izquierda
-    def posibles(self):
-        return self.caminos
-    def test_sinportal(self,matriz):
+    def buscar_cambio(self,letra,pos):
+        for i in range(len(self.matriz)):
+            if letra in self.matriz[i]:
+                if i!=pos[0] and self.matriz[i].index(letra)!=pos[1]:
+                    return (i,self.matriz[i].index(letra))
+        return False
+
+
+    def caminos(self,nodo_fin,nodo_inicio,info,mov): #caminos mas cortos de una posicion X a la posicion Y
+        i=nodo_inicio.pos
+        j=nodo_inicio.pos
+        info[i].append([i,j])
+        tablero = self.matriz
+        mov+=" "+str(i)+","+str(j)
+        buscar=search(nodo_inicio,nodo_fin)
+
+        if tablero[i][j] != info[0] and (i>0 or i<len(tablero)) and (j>0 or j<len(tablero)) and tablero[i][j] != "#":
+        
+            if buscar[0]!=0 and i+buscar[0] < len(tablero) and [i+buscar[0],j] not in info[1]:
+                self.caminos(nodo_fin,[i+buscar[0],j],info,mov)
+            if buscar[1]!=0 and j+buscar[1] < len(tablero) and [i,j+buscar[1]] not in info[1]:
+                self.caminos(nodo_fin,[i,j+buscar[1]],info,mov)
+
+        if tablero[i][j]==info[0] and (info[0] not in info[-1].sig or len(mov.split(" ")-1)<info[-1].sig_peso[info[0]]):
+            a=mov.split(" ")
+            print("entro")
+            info[-1].add_next(info[0],len(a)-1)
+
+    def caminos_cortos(self,pos):
+        visitar = []
+        for key in self.portales:
+            a = self.portales[key][0].pos[0]-pos[0]
+            b = self.portales[key][0].pos[1]-pos[1]
+            c = (a**2)**0.5+(b**2)**0.5
+            a = self.portales[key][1].pos[0]-pos[0]
+            b = self.portales[key][1].pos[1]-pos[1]
+            d = (a**2)**0.5+(b**2)**0.5
+            if c<d:
+                visitar.append(self.portales[key][0])
+            else:
+                visitar.append(self.portales[key][1])
+        return visitar
+
+    def buscar_conexiones(self):
+        for portal in self.portal_list:
+            self.caminos(self.portales[portal][0],self.inicio,["S",[],test.inicio],"")
+            self.caminos(self.portales[portal][1],self.inicio,["S",[],test.inicio],"")
+        for key in self.portal_list:
+            for portal in self.portal_list[key]:
+                for portal_buscado in self.portal_list:
+                    if key!=portal_buscado and (self.portales[portal][0] not in portal.sig or self.portales[portal][1] not in portal.sig):
+                        self.caminos(portal_buscado[0],portal,["S",[],test.inicio],"")
+                        self.caminos(portal_buscado[1],portal,["S",[],test.inicio],"")
+        for portal in self.portal_list:
+            self.caminos(self.salida, self.portales[portal][0],["S",[],test.inicio],"")
+            self.caminos(self.salida, self.portales[portal][1],["S",[],test.inicio],"")
+
+    def buscar_salida(self):
         pass
-    def test_gota(self,matriz):
-        evaluar=[self.inicio]
-        evaluados=[]
-        for i in range(len(tablero)):
-            for j in range(len(tablero)):
-                if tablero[i][j] == 0 and (i>0 or i<len(tablero)) and (j>0 or j<len(tablero)):
-                    for k in range(-1,2):
-                        for l in range(-1,2):
-                            try:
-                                if tablero[i+k][j+l] == "." and (i+k > -1 and j+l > -1):
-                                    tablero[i+k][j+l] = minefield[i+k][j+l]
-                                    band=True
-                            except IndexError:
-                                pass
-            if band:
-                cero(tablero,minefield)
 
 
 
+filas=open("laberinto.txt", "r")
+target = []
+for linea in filas.readlines():
+    linea = linea.replace(".\n",".")
+    target.append(str(linea).split(" "))
+filas.close
 
 
+test = laberinto(target)
+test.buscar_conexiones
 
-
-matriz='''. . . A . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-. . . . . B . . . . . . . . .
-. . . . . . . . . . . . . . .
-. . . . . . . . . C . . . . .
-. . . . . . . D . . . . . . E
-. . . . . . . . . . . . . . .
-. . . . F . . . . . G . . . .
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-. H . . . . . X . . . . . I .
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-. . . . J . . . . . K . . . .
-. . . . . . . . . . . . . . .
-L . . . . . . M . . . . . . .
-. . . . . N . . . . . . . . .
-. . . . . . . . . . O . . . .
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . .
-. . . . . . . . . . . P . . .'''
-
-
+print(test.inicio.sig)
+print(test.inicio.sig_peso)
+print(test.portales["a"][0].sig)
