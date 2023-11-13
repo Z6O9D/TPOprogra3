8c352035e24dc05411c4a5f1086c2e8ae3e239c6
fedese08 <fedesena2000@gmail.com>
2023-11-12 23:10:05 -0300

    cambios finales

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # TPOprogra3
+
 Estrategia:
-comparar el camino mas corto entre todos los portales teniendo en cuenta su distancia a la salida y a la entrada con el camino a la entrada a la salida para ver de que forma se llega mas rapido y el resultado es la solución final. Cada uno haga un intento de solucion en su rama.
-Gracias
+
+El codigo utiliza un algoritmo de busqueda en amplitud (BFS) para encontrar el camino mas corto en un laberinto desde una entrada a una salida, teniendo en cuenta los portales que pueden transportarnos a diferentres partes del laberinto.
+
+La estrategia fue la siguiente:
+Se recorre cada celda del laberinto para encontrar la entrada, la salida y los portales. Si encuentra un portal lo agrega al diccionario de portales, con las coordenadas de su respectiva ubicacion usando la letra del portal como clave. 
+Una vez encontrados la entrada, la salida y todos los portales, se inicia una busqueda en amplitud. Comienza en la entrada y explora todas las celdas adyacentes que no sean paredes (#) y que no hayan sido visitadas antes. Si la celda acual es un portal, se agregan todas las otras ubicaciones de ese portal al final de la cola. Esto nos permite saltar a traves de los portales. Si la celda actual es la salida, el codigo devuelve la cantidad de pasos que se dieron hasta llegar a ella. En cambio si el algoritmo recorre todo el laberinto y no encuentra una salida devuelve "-1", indicando que no se encontro un camino de la entrada a la salida.
+
+Complejidad:
+
+O( i * j + (n - m) ) 
+Donde i y j son las dimensiones del laberinto y n son la cantidad de casillas posibles  y p son las paredes 
--- buscando.py
+++ buscando.py
@@ -1,11 +1,11 @@
 # Solucion planteada :
 
-def solve(maze):
+def solve(maze): # O( i * j + (n - m) ) donde i y j son las dimensiones del laberinto y n son la cantidad de casillas posibles  y p son las paredes 
     # Encuentra la entrada, la salida y los portales
     entrada = None
     salida = None
     portales = {}
-    for i in range(len(maze)):
+    for i in range(len(maze)):  # O( i * j )
         for j in range(len(maze[0])):
             if maze[i][j] == 'E':
                 entrada = (i, j)
@@ -19,31 +19,31 @@ def solve(maze):
     # BFS ( Busqueda en amplitud ) desde la entrada
     queue = [(entrada, 0)]
     visited = {entrada}
-    while queue:
+    while queue: # Mientras la cola no este vacia           O( n - m )
         (x, y), steps = queue.pop(0)  # Usamos pop(0) para eliminar el primer elemento
         if (x, y) == salida:
             return steps  # Encontramos la salida
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            nx, ny = x + dx, y + dy
-            if (0 <= nx < len(maze)) and (0 <= ny < len(maze[0])) and (maze[nx][ny] != '#') and ((nx, ny) not in visited):
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]: # Movimientos posibles    
+            nx, ny = x + dx, y + dy # Nueva posicion
+            if (0 <= nx < len(maze)) and (0 <= ny < len(maze[0])) and (maze[nx][ny] != '#') and ((nx, ny) not in visited): # Si no es pared y no hemos visitado la posicion
                 visited.add((nx, ny))
                 queue.append(((nx, ny), steps + 1))
                 if maze[nx][ny].islower():  # Si es un portal
-                    for portal in portales[maze[nx][ny]]:
-                        if portal not in visited:
+                    for portal in portales[maze[nx][ny]]: # Buscamos el otro portal
+                        if portal not in visited:  
                             visited.add(portal)
-                            queue.append((portal, steps + 2))
+                            queue.append((portal, steps + 2)) # Sumamos 2 pasos por el portal
     return -1  # No hay camino a la salida
 
 
 maze = [
 
     # Maze 1
-    # ['S', '.', '.', '.',],
-    # ['#', '#', '#', '.'],
-    # ['.', '.', '.', '.',],
-    # ['.', '#', '#', '#'],
-    # ['.', '.', '.', 'E']
+    ['.', '.', '.', '.',],
+    ['#', '#', '#', '.'],
+    ['.', 'S', '.', '.',],
+    ['.', '#', '#', '.'],
+    ['.', '.', '.', 'E']
 
     # Maze 2
     # ['E', '.', '.'],
@@ -57,12 +57,12 @@ maze = [
     
 
     # Maze 4
-    ['S', '.', 'b', '#', 'b'],
-    ['#', '#', '#', '#', 'a'],
-    ['.', '.', 'E', '#', '#'],
-    ['c', '#', '#', '.', 'c'],
-    ['#', 'a', '.', '.', '.']
+    # ['S', '.', 'b', '#', 'b'],
+    # ['#', '#', '#', '#', 'a'],
+    # ['.', '.', 'E', '#', '#'],
+    # ['c', '#', '#', '.', 'c'],
+    # ['#', 'a', '.', '.', '.']
 ]
 
 
-print(solve(maze))
+print("Se necesitan ",solve(maze), " pasos para llegar a la salida")
